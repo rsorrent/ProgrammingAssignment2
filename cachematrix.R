0b615ba9f450e46566b4732ef523ab3f3081c58c
rsorrent <riccardo.sorrentino@ilsole24ore.com>
2014-08-20 09:54:57 +0200

    A minor typo

--- cachematrix.R
+++ cachematrix.R
@@ -1,13 +1,13 @@
-##Those functions allow to cache the result of the calculation 
-##of the inverse of a matrix x in an environment different from the current environment
-##So, instead of recomputing the inverse of x, we can recover the result from the cache, saving time.
+## These functions allow to cache the result of the calculation 
+## of the inverse of a matrix x in an environment different from the current environment
+## So, instead of recomputing the inverse of x, we can recover the result from the cache, saving time.
 
 
 ## makeCacheMatrix create a list cointaining a function to 
-##(1) set the value of the matrix,
-##(2) get the value of the matrix,
-##(3) set the value ot the inverse matrix
-##(4) get the value of the inverse matrix
+## (1) set the value of the matrix,
+## (2) get the value of the matrix,
+## (3) set the value ot the inverse matrix
+## (4) get the value of the inverse matrix
 
 
 makeCacheMatrix <- function(x = matrix())
@@ -27,8 +27,8 @@ makeCacheMatrix <- function(x = matrix())
         }
 
 ## cachesolve checks if the inverse of the matrix created with macheCacheMatrix
-##has already been calculated. In this case it gets the inverse from the cache. Otherwise 
-##it calculate the inverse of that matrix and sets it in the cache
+## has already been calculated. In this case it gets the inverse from the cache. Otherwise 
+## it calculate the inverse of that matrix and sets it in the cache
 
 cachesolve <- function(x, ...) 
         {
@@ -44,3 +44,4 @@ cachesolve <- function(x, ...)
                 m
         }
         
+
